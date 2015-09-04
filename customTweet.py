a4bfada9d40c481edded8ec015dc5ff9919440d2
Anudeep Bulla <anudeepb@buffalo.edu>
2015-09-04 23:42:12 +0000

    add code to pull tweets and add to solr

---
+++ customTweet.py
@@ -0,0 +1,93 @@
+
+import json
+import urllib
+
+
+class customTweet :
+    
+    _id = '0'               #user_id of the tweeter
+    lang = 'en'             #language of the tweeet
+    created_at = ''         #UTC time when the tweet was posted
+    coordinates = [-1,-1]   #[longitude,latitude]
+    text = ''               #text of tweeet
+    
+    lang_set_en = ['en','en-us','en-gb','en-au','en-ca','en-nz','en-ie','en-za','en-jm','en-bz','en-tt','en-in']
+    lang_set_de = ['de','de-ch','de-at','de-li','de-lu']
+    lang_set_ru = ['ru','ru-mo']
+    
+    term_set = ['health']
+
+    
+    def __init__(self, text):
+    
+        try:
+            json_text = json.loads(text)
+            self._id = json_text['id_str']
+        except KeyError:
+            pass
+        
+        try:
+            self.lang = json_text['lang']
+        except KeyError:
+            pass
+        
+        try:
+            self.created_at = json_text['created_at']
+        except KeyError:
+            pass
+        
+        try:
+            self.coordinates = json_text['coordinates']['coordinates']
+            if not self.coordinates:
+                self.coordinates = [-1,-1]
+        except KeyError:
+            self.coordinates = [-1,-1]
+        except TypeError:
+            self.coordinates = [-1,-1]
+        
+        try:
+            self.text = json_text['text']
+        except KeyError:
+            pass
+
+
+    def set_vals(self, _id='0',lang='en',created_at='',coordinates=[-1,-1],text=''):
+        self._id = _id
+        self.lang = lang
+        self.created_at = created_at
+        self.coordinates = coordinates
+        self.text = text
+
+    def is_lang_english (self):
+        #global lang_set_en
+        return self.lang in self.lang_set_en
+        
+    def is_lang_german (self):
+        #global lang_set_de
+        return self.lang in self.lang_set_de
+  
+    def is_lang_russian (self):
+        #global lang_set_ru
+        return self.lang in self.lang_set_ru
+    
+    def is_lang_interesting (self):
+        return self.is_lang_english() or self.is_lang_german() or self.is_lang_russian()
+    
+    
+    def is_term_interesting (self):
+        for term in self.term_set:
+            return term in self.text
+        return false
+    
+    
+    def encode_to_json (self):
+        str_tweet = '[{'
+        str_tweet += ' "id" : "'+ self._id + '",'
+        str_tweet += ' "lang" : "'+ self.lang + '",'
+        str_tweet += ' "created_at" : "'+ self.created_at + '",'
+        str_tweet += ' "coordinates" : { "longitude" : '+ str(self.coordinates[0]) + ', "latitude" :' + str(self.coordinates[1]) + '},'
+        str_tweet += ' "text" : "'+ urllib.parse.quote_plus(self.text) + '"'
+        str_tweet += '}]'
+        
+        #print(json.loads(str_tweet))
+        return str_tweet
---
+++ twitterSiphon.py
@@ -0,0 +1,76 @@
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+import json
+import requests
+import sys
+from customTweet import customTweet
+
+ckey = 'ZiS77nnXgwbPIVZ5AsvxERCnK'
+csecret = 'VKcf7Sfd5mwmpcjSuGOWVCkiLMfpoMOZ5i4LLidCZtwhMUBv8Q'
+atoken = '3526839621-Poj6uaa1CNn5WPXK3GFD91NMs0Do1WON3yxkUpE'
+asecret = 'Fo0YlmTqMnLbeH3GsgNn1VyEmCIW3IGWuDKDrQ6cWWXD3'
+
+count = 1
+interesting_count=1
+update_url = ['http://localhost:8983/solr/gettingstarted/update/json/docs','http://localhost:7574/solr/gettingstarted/update/json/docs']
+update_url_args = ['?split=/'+ \
+                  '&f=id:/id' + \
+                  '&f=lang:/lang' + \
+                  '&f=created_at:/created_at' + \
+                  '&f=longitude:/coordinates/longitude' + \
+                  '&f=latitude:/coordinates/latitude' + \
+                  '&f=text:/text'+ \
+                  '&commit=true'+ \
+                  '&data=' , \
+                  \
+                  '?split=/'+ \
+                  '&f=id:/id' + \
+                  '&f=lang:/lang' + \
+                  '&f=created_at:/created_at' + \
+                  '&f=longitude:/coordinates/longitude' + \
+                  '&f=latitude:/coordinates/latitude' + \
+                  '&f=text:/text'+ \
+                  '&data='
+                  ]
+
+headers = {'Content-type':'application/json'}
+
+
+class twitterListener(StreamListener) :
+
+    def on_data(self, data) :
+        global count
+        global interesting_count
+        tweet = customTweet(data)
+        
+        if tweet.is_lang_interesting() and tweet.is_term_interesting():
+            print("Got a new tweet :: Total # : "+ str(interesting_count)+"/"+str(count))
+            interesting_count+=1
+            if interesting_count <= 50:
+                req = requests.post(update_url[count%2]+update_url_args[0 if count%25==0 else 1], data = tweet.encode_to_json(), headers=headers)
+                #print(req.text)
+                #print("Pushing to SOLR : return# "+str(req.status_code))
+            else:
+                '''
+                    commit both cores. One duplicate tweet will be added to one of the core, but shouldn't matter over the other count
+                '''
+                req = requests.post(update_url[1]+update_url_args[0], data = tweet.encode_to_json(), headers=headers)
+                req = requests.post(update_url[0]+update_url_args[0], data = tweet.encode_to_json(), headers=headers)
+                sys.exit(0)
+        else:
+            print("Unkown or uninteresting language/term, skipping")
+        #print("Got a new tweet :: "+parsed_text['text'].encode('ascii', 'ignore').decode('ascii')+"\nTotal # : "+ str(count))
+        count = count + 1
+        return True
+        
+    def on_error(self, status) :
+        print(status)
+        
+
+auth = OAuthHandler(ckey,csecret)
+auth.set_access_token(atoken,asecret)
+
+twitterStream = Stream(auth,twitterListener())
+twitterStream.filter(track=['health'])
