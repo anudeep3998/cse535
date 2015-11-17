3b0f3cf9d9ab1494656cbcb8b04b85c2e174ac7b
Anudeep Bulla <anudeepb@buffalo.edu>
2015-11-17 00:08:06 +0000

    final vals

--- projB/test/json_to_trec.py
+++ projB/test/json_to_trec.py
@@ -15,7 +15,7 @@ import subprocess
 
 # change the url according to your own koding username and query
 #inurl = 'http://anudeep3998.koding.io:8983/solr/vsm/select?q=%D0%A0%D0%A4+%D0%B2+%D0%A1%D0%B8%D1%80%D0%B8%D0%B8+%D0%B2%D1%8B%D0%BD%D1%83%D0%B4%D0%B8%D0%BB%D0%B8+250+%D1%82%D1%83%D0%BD%D0%B8%D1%81%D1%81%D0%BA%D0%B8%D1%85+%D0%B1%D0%BE%D0%B5%D0%B2%D0%B8%D0%BA%D0%BE%D0%B2+%D0%B1%D0%B5%D0%B6%D0%B0%D1%82%D1%8C%0A&wt=json&indent=true&defType=dismax&qf=text_ru&fl=id%2Cscore&wt=json&indent=true&rows=1000'
-outfn = 'result2.txt'
+outfn = 'result.txt'
 
 queries = []
 
@@ -30,7 +30,17 @@ res = os.popen("curl 'http://localhost:8983/solr/vsm/update/json?commit=true' --
 
 infn = "/home/anudeep3998/cse535/code/projB/new_queries.txt"
 q_count = 0
+
+
+#bm : 
+#custom_params = "qf=text_dump^1+tweet_hashtags^0.9&mm=1&pf=text_dump"
+#lm : 
+#custom_params = "qf=text_en^1+text_ru^0.8+text_de^1+tweet_hashtags^0.6&mm=1&pf=text_dump"
+#vsm : 
 custom_params = "qf=text_en^1+text_ru^0.7+text_de^1.3+tweet_hashtags^0.3&mm=1&pf=text_dump"
+
+
+
 similarity = "vsm"
 
 logger_f = similarity+'/logger.txt'
